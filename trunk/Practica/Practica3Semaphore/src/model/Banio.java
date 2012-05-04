1581e19a524fc0d3e3437ae20257d7b8f1791204
alejandromelo2005@gmail.com <alejandromelo2005@gmail.com@643c6226-7cab-89d6-4db0-3cc11492b20a>
2012-05-04 04:06:51 +0000

    funciona Lo De Los Baños pero Se Produce Un DeadLock cada tanto

--- trunk/Practica/Practica3Semaphore/src/model/Banio.java
+++ trunk/Practica/Practica3Semaphore/src/model/Banio.java
@@ -6,6 +6,8 @@ public class Banio {
 
 	//Variables
 	private Semaphore toilets;
+	private Semaphore mutex;
+	private Semaphore mutexE;
 	private Semaphore solo; //para hacer exclucion de solo hombres o solo mujeres.
 	private Integer cantHombres;
 	private Integer cantMujeres;
@@ -16,6 +18,7 @@ public class Banio {
 		this.setCantMujeres(0);
 		this.setToilets(new Semaphore(cantidadToilets , true));
 		this.solo= new Semaphore(1);
+		this.mutex= new Semaphore(1);
 	}
 	/**
 	 * Retorna True si no hay ninguna Mujer En el banio.
@@ -35,29 +38,64 @@ public class Banio {
 	 * Ingresa un Hombre.
 	 */
 	public void ingresaAlBanioUnHombre(){ //Ver Esto
+		if(this.cantHombres==0){
+			try {
+				this.solo.acquire();
+				this.getToilets().acquireUninterruptibly();
+				this.cantHombres+=1;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		else{
 			this.getToilets().acquireUninterruptibly();
 			this.cantHombres+=1;
+		}
 	}
 	/**
 	 * Sale Del Baño un Hombre.
 	 */
 	public void saleDelBanioUnHombre(){
+		this.mutex.acquireUninterruptibly();
 		this.cantHombres-=1;
+		this.mutex.release();
+		if(this.cantHombres == 0){
+			this.solo.release();
+			}
 		this.getToilets().release();
+		
 	}
 	/**
 	 * Ingresa una Mujer.
 	 */
 	public void ingresaAlBanioUnaMujer(){
-		this.getToilets().acquireUninterruptibly();
-		this.cantMujeres+=1;
+		this.mutex.acquireUninterruptibly();
+		if(this.cantMujeres==0){
+			try {
+				this.solo.acquire();
+				this.getToilets().acquireUninterruptibly();
+				this.cantMujeres+=1;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		else{
+			this.getToilets().acquireUninterruptibly();
+			this.cantMujeres+=1;
+		}
+		this.mutex.release();
 	}
 	/**
 	 * Sale Del banio una Mujer.
 	 */
 	public void saleDelBanioUnaMujer(){
+		this.mutex.acquireUninterruptibly();
 		this.cantMujeres-=1;
+		if(this.cantMujeres == 0){
+			this.solo.release();
+			}
 		this.getToilets().release();
+		this.mutex.release();
 	}
 	
 	//Getters & Setters
--- trunk/Practica/Practica3Semaphore/src/model/Mujer.java
+++ trunk/Practica/Practica3Semaphore/src/model/Mujer.java
@@ -3,8 +3,8 @@ package model;
 public class Mujer extends Persona {
 
 	// Constructor
-	public Mujer(String nombre, Banio baño) {
-		this.setBanio(baño);
+	public Mujer(String nombre, Banio banio) {
+		this.setBanio(banio);
 		this.setNombre(nombre);
 	}
 
@@ -15,10 +15,10 @@ public class Mujer extends Persona {
 	public void run() {
 		while (true) {
 			this.getBanio().ingresaAlBanioUnaMujer();
-			System.out.println(this.getNombre() + " Entro al Ba�o");
+			System.out.println(this.getNombre() + " Entro al Banio");
 			// Seccion Critica
 			this.getBanio().saleDelBanioUnaMujer();
-			System.out.println(this.getNombre() + " Salio del Ba�o");
+			System.out.println(this.getNombre() + " Salio del Banio");
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
